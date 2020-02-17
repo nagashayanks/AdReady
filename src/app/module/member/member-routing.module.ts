c8de042934f2c9eb4b0f8228815bd8e73136846b
nagashayanks <user1@LP-5CD9176M8P.HCLT.CORP.HCL.IN>
2020-02-17 11:15:07 +0530

    routing added

--- src/app/module/member/member-routing.module.ts
+++ src/app/module/member/member-routing.module.ts
@@ -1,21 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 import { MemberComponent } from './member/member.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: MemberComponent
+    component: LoginComponent
   },
   {
     path: 'login',
     component: LoginComponent
-  },
-  {
-    path: 'register',
-    component: RegisterComponent
   }
 ];
 @NgModule({
